30354e6ead64778dc720e29824ba9ac0fa4745a4
pspkzar <cesar.perdigao92@gmail.com>
2013-05-30 17:54:22 +0100

    noblock

--- EX3/interface.pl
+++ EX3/interface.pl
@@ -38,7 +38,7 @@
 
 demo( Agente,Questao ) :-
     out(demo(Agente,Questao)),
-	in(prova(A,X)),
+	in_noblock(prova(A,X)),
 	write( ( X ) ),nl.
 %--------------------------------- - - - - - - - - - -  -  -  -  -   -
 
